161bb9c6085db75e4b7c77544e34af1cc055af01
FIO-PC123\USER <mindy8503020312@gmail.com>
2017-05-19 17:05:47 +0800

    add verify

---
+++ app/src/main/java/com/example/carrie/carrie_test1/certify.java
@@ -0,0 +1,14 @@
+package com.example.carrie.carrie_test1;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class certify extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_certify);
+    }
+
+}
