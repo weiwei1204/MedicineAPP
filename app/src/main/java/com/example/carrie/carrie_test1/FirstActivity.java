c71e96b8d74462d906f966324d07ea24b721965d
Rita <baby1204w@gmail.com>
2017-05-23 00:32:11 +0800

    藥品詳細資料介面ok

--- app/src/main/java/com/example/carrie/carrie_test1/FirstActivity.java
+++ app/src/main/java/com/example/carrie/carrie_test1/FirstActivity.java
@@ -1,5 +1,6 @@
 package com.example.carrie.carrie_test1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,6 +12,11 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+
+    public void gotoFourthActivity(View v){ //連到搜尋藥品資訊頁面
+        Intent it = new Intent(this,FourthActivity.class);
+        startActivity(it);
     }
     public void goback(View v){
         finish();
