f25645dadef6f2075866a05a925b0ae13bb2a56e
Rita <baby1204w@gmail.com>
2017-05-23 00:59:12 +0800

    選擇日期功能

---
+++ app/src/main/java/com/example/carrie/carrie_test1/FourthActivity.java
@@ -0,0 +1,19 @@
+package com.example.carrie.carrie_test1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class FourthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourth);
+    }
+    public void goback(View v){
+        finish();
+    }
+
+}
