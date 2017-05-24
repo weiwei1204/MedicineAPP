617ee35efc4bd0ba3d3f0baeab670b3d4b5b0c04
Shana <shana850317@gmail.com>
2017-05-24 16:38:24 +0800

    third

---
+++ app/src/main/java/com/example/carrie/carrie_test1/medicineverify.java
@@ -0,0 +1,14 @@
+package com.example.carrie.carrie_test1;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class medicineverify extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicineverify);
+    }
+
+}
