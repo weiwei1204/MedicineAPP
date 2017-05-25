3046fee094da53de003d525c8c077847e5bfb67b
Shana <shana850317@gmail.com>
2017-05-25 11:53:25 +0800

    comment/history/vertify

--- app/src/main/java/com/example/carrie/carrie_test1/comment.java
+++ app/src/main/java/com/example/carrie/carrie_test1/comment.java
@@ -2,13 +2,20 @@ package com.example.carrie.carrie_test1;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
-public class medicineverify extends Activity {
+public class comment extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_medicineverify);
+        setContentView(R.layout.activity_comment);
     }
+
+    public void goback(View v){
+        finish();
+    }
+
+
 
 }
