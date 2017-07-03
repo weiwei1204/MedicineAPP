41318f9a6514ab56d4d0002fdffa9a7df9712430
Shana <shana850317@gmail.com>
2017-07-03 14:12:16 +0800

    更新

--- app/src/main/java/com/example/carrie/carrie_test1/Main2.java
+++ app/src/main/java/com/example/carrie/carrie_test1/Main2.java
@@ -5,18 +5,43 @@ import android.app.Activity;
 import android.os.Handler;
 import android.content.Intent;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Main2 extends Activity {
 
-
     private static int SPLASH_TIME_OUT=4000;
-
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main2);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.main2_animation);
+        imageView.setAnimation(animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                finish();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
