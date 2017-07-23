05d627191638569558752066c29176966ce4dbdb
Rita <baby1204w@gmail.com>
2017-07-23 19:46:43 +0800

    Nothing

--- app/src/main/java/com/example/carrie/carrie_test1/Main2.java
+++ app/src/main/java/com/example/carrie/carrie_test1/Main2.java
@@ -2,19 +2,11 @@ package com.example.carrie.carrie_test1;
 
 import android.app.Activity;
 import android.content.Intent;
-
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 import android.os.Bundle;
 import android.os.Handler;
 
 
-public class Main2 extends Activity {
 
-    private static int SPLASH_TIME_OUT=4000;
-   // ImageView imageView;
 
 
 public class Main2 extends Activity {
@@ -36,11 +28,11 @@ public class Main2 extends Activity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(Main2.this, beaconActivity.class);
+                Intent homeIntent = new Intent(Main2.this, LoginActivity.class);
                 startActivity(homeIntent);
                 finish();
             }
         }, SPLASH_TIME_OUT);
     }
 
-}}
+}
