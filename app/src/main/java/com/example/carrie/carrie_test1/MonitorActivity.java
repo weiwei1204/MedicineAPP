7cf47f56f49e6d883e82d6867c36e30bb11db542
Ｍindy <mindy8503020312@gmail.com>
2017-07-31 02:28:56 +0800

    監視toolbar

--- app/src/main/java/com/example/carrie/carrie_test1/MonitorActivity.java
+++ app/src/main/java/com/example/carrie/carrie_test1/MonitorActivity.java
@@ -2,13 +2,38 @@ package com.example.carrie.carrie_test1;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.util.Log;
 
-public class MonitorActivity extends Activity {
+public class MonitorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitor);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_monitor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.action_add: //點了重新整理
+                return true;
+            case R.id.action_settings: //點了settings
+                Log.d("item","click settings");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
