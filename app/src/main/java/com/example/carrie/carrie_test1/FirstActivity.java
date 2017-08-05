c0ba85fa674aa8b24f5471fe8b4390e0adbb5eb1
YuanMaSa <jetlag10030226@gmail.com>
2017-08-05 22:46:34 +0800

    test

--- app/src/main/java/com/example/carrie/carrie_test1/FirstActivity.java
+++ app/src/main/java/com/example/carrie/carrie_test1/FirstActivity.java
@@ -2,53 +2,159 @@ package com.example.carrie.carrie_test1;
 
 import android.app.SearchManager;
 import android.content.Context;
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuInflater;
-
-// 注意這裡, Android Studio 預設會幫您引入 import android.widget.SearchView
-// 但我們要的是 android.support.v7.widget.SearchView;
-import android.support.v7.widget.SearchView;
-import android.widget.AdapterView;
+import android.view.View;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
+import android.widget.Toast;
+import android.content.Intent;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 
 public class FirstActivity extends AppCompatActivity {
 
-    String getDrugUrl = "http://54.65.194.253/Drug/GetDrug.php";
-    RequestQueue requestQueue;
-    TextView chname;
+    private String TAG = FirstActivity.class.getSimpleName();
+//    private String getdata;
+    private ListView lv;
+//    private EditText sendchname;
+//    private EditText sendengname;
+//    ArrayAdapter<String> drugadapter;
+
+
+    ArrayList<HashMap<String, String>> contactList;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
-        chname=(TextView)findViewById(R.id.chname);
-        getDrug();
+        contactList = new ArrayList<>();
+//        sendchname = (EditText) findViewById(R.id.chname);
+//        sendengname = (EditText) findViewById(R.id.engname);
+        lv = (ListView) findViewById(R.id.list);
+
+        new GetContacts().execute();
+
 
     }
 
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(FirstActivity.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
 
-    public void gotoFourthActivity(View v){ //連到搜尋藥品資訊頁面
+        }
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+            // Making a request to url and getting response
+            String url = "http://54.65.194.253/test/testDrugAll.php";
+            String jsonStr = sh.makeServiceCall(url);
+            Log.d("123","0");
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                Log.d("123","1");
+                try {
+                    Log.d("123","1.5");
+                       Log.d("123","1");
+                    // Getting JSON Array node
+                    JSONArray  drugs = new JSONArray(jsonStr);
+                       Log.d("123","2");
+                    // looping through All Contacts
+                    for (int i = 0; i < drugs.length(); i++) {
+
+                        JSONObject c = drugs.getJSONObject(i);
+                        String id = c.getString("id");
+                        String chineseName = c.getString("chineseName");
+                        String englishName = c.getString("englishName");
+                        String image = c.getString("image");
+                        Log.d("123","3");
+
+
+
+                        // Phone node is JSON Object
+
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> druginfo = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        druginfo.put("id", id);
+                        druginfo.put("chineseName", chineseName);
+                        druginfo.put("englishName", englishName);
+                        druginfo.put("image",image);
+                        Log.d("123","4");
+                        // adding contact to contact list
+                        contactList.add(druginfo);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+        protected void showdrugdata(){
+
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+//            ListAdapter adapter = new SimpleAdapter(FirstActivity.this, contactList,
+//                    R.layout.activity_first, new String[]{ "chineseName","englishName"},
+//                    new int[]{R.id.chname,R.id.engname});
+//            lv.setAdapter(adapter);
+
+
+        }
+    }
+
+
+
+
+        public void gotoFourthActivity(View v){ //連到搜尋藥品資訊頁面
         Intent it = new Intent(this,FourthActivity.class);
         startActivity(it);
     }
@@ -60,6 +166,7 @@ public class FirstActivity extends AppCompatActivity {
     public void goback(View v){
         finish();
     }
+
 
 
     @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
@@ -80,45 +187,5 @@ public class FirstActivity extends AppCompatActivity {
         return true;
     }
 
-    public void getDrug(){//取此會員的beacon
-        Log.d("aaa","4");
-
-        requestQueue = Volley.newRequestQueue(getApplicationContext());
-        Log.d("aaa","3");
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,getDrugUrl, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    Log.d("aaa","1");
-
-                    JSONArray drugs = response.getJSONArray("Drugs");
-
-                    final String[] drugarray=new String[drugs.length()];
-                    final String[] drugaidrray=new String[drugs.length()];
-                    for (int i=0 ; i<drugs.length() ; i++){
-                        Log.d("aaa","2");
-                        JSONObject drug = drugs.getJSONObject(i);
-                        String chinesename = drug.getString("chineseName");
-                        String indication = drug.getString("indication");
-                        String id = drug.getString("");
-                            drugaidrray[i] = id;
-                            drugarray[i] = chinesename;
-                            Log.d("vvvvv",drugarray[i]);
-                    }//取值結束
-//                    chname.setText(drugarray[0]);
-//                    Log.d("aaa",drugarray[0]);
-                }catch (JSONException e){
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-            }
-
-        });
-        requestQueue.add(jsonObjectRequest);
-    }
 
 }
