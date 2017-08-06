b8851016ef5a22266af79eb488cd1d5dc3b7ed8e
YuanMaSa <jetlag10030226@gmail.com>
2017-08-06 18:54:45 +0800

    no message

--- app/src/main/java/com/example/carrie/carrie_test1/SwipePlot.java
+++ app/src/main/java/com/example/carrie/carrie_test1/SwipePlot.java
@@ -69,6 +69,7 @@ public class SwipePlot extends AppCompatActivity {
     }
 
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
