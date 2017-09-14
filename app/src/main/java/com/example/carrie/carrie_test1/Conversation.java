13ad874a1d6eaacd280946656da5af83aedd6e74
YuanMaSa <jetlag10030226@gmail.com>
2017-09-14 14:18:46 +0800

    update onBackPressed issue and add firebase testing and Lex testing

---
+++ app/src/main/java/com/example/carrie/carrie_test1/Conversation.java
@@ -0,0 +1,32 @@
+package com.example.carrie.carrie_test1;
+
+/**
+ * Created by jonathan on 2017/9/13.
+ */
+import java.util.ArrayList;
+import java.util.List;
+public class Conversation {
+    private static List<TextMessage> currentConversation;
+
+    public static void clear() {
+        currentConversation = new ArrayList<TextMessage>();
+    }
+
+    public static void add(final TextMessage message) {
+        if (currentConversation == null) {
+            clear();
+        }
+        currentConversation.add(message);
+    }
+
+    public static TextMessage getMessage(final int pos) {
+        if (currentConversation == null) {
+            return null;
+        }
+        return currentConversation.get(pos);
+    }
+
+    public static int getCount() {
+        return currentConversation == null ? 0 : currentConversation.size();
+    }
+}
